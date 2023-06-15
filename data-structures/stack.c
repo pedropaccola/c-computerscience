a255b5a34f043ace04c22c09437ea41b83d78bac
Pedro Paccola <paccolapedro@gmail.com>
2023-06-15 14:36:15 -0300

    stack with bracked notation

--- data-structures/stack.c
+++ data-structures/stack.c
@@ -25,13 +25,17 @@ bool IsFull(stack_t *stack) {
 
 void Push(stack_t *stack, int value) {
   printf("Push element %d\n", value);
+
   if (IsFull(stack)) {
     printf("Stack is full\n");
     return;
   }
   stack->top++;
-  int *ptr = stack->elements;
-  *(ptr + stack->top) = value;
+
+  stack->elements[stack->top] = value;
+  // OR:
+  // int *ptr = stack->elements;
+  // *(ptr + stack->top) = value;
 }
 
 void Pop(stack_t *stack) {
@@ -39,9 +43,13 @@ void Pop(stack_t *stack) {
     printf("Stack is empty\n");
     return;
   }
-  int *ptr = stack->elements;
-  printf("Pop element %d\n", *(ptr + stack->top));
-  *(ptr + stack->top) = 0;
+
+  printf("Pop element %d\n", stack->elements[stack->top]);
+  stack->elements[stack->top] = 0;
+  // OR:
+  // int *ptr = stack->elements;
+  // printf("Pop element %d\n", *(ptr + stack->top));
+  // *(ptr + stack->top) = 0;
   stack->top--;
 }
 
@@ -50,15 +58,22 @@ int Peek(stack_t *stack) {
     printf("Stack is empty\n");
     return -1;
   }
-  int *ptr = stack->elements;
-  return *(ptr + stack->top);
+
+  return stack->elements[stack->top];
+  // OR:
+  // int *ptr = stack->elements;
+  // return *(ptr + stack->top);
 }
 
 void PrintStack(stack_t *stack) {
   printf("top: %d, elements: ", stack->top);
+
   for (int i = 0; i <= stack->top; i++) {
-    printf("%d, ", *(stack->elements + i));
+    printf("%d, ", stack->elements[i]);
+    // OR:
+    // printf("%d, ", *(stack->elements + i));
   }
+
   printf("\n\n");
 }
 
